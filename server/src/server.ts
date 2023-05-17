07e06c2dbe423489ba96a40866f0710afa4f4f93
Vinicius Colodetti <vinicius.colodetti@gmail.com>
2023-05-16 22:40:44 -0300

    Segunda Aula

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,13 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
+app.register(memoriesRoutes)
 
-  return users
+app.register(cors, {
+  origin: true,
 })
 
 app
