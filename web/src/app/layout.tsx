70dc09ad6063c63c3872cf6e51ff09712c139d27
Vinicius Colodetti <vinicius.colodetti@gmail.com>
2023-05-21 11:22:23 -0300

    Quarta Aula

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -4,6 +4,13 @@ import {
   Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamjuree,
 } from 'next/font/google'
+import { Blur } from '@/components/Blur'
+import { Hero } from '@/components/Hero'
+import { Profile } from '@/components/Profile'
+import { SignIn } from '@/components/SignIn'
+import { Stripes } from '@/components/Stripes'
+import { Copyright } from '@/components/Copyright'
+import { cookies } from 'next/headers'
 
 const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
 const baiJamjuree = BaiJamjuree({
@@ -19,12 +26,27 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has('token')
+
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            <Blur />
+            <Stripes />
+
+            {isAuthenticated ? <Profile /> : <SignIn />}
+            <Hero />
+            <Copyright />
+          </div>
+
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   )
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,30 +1,5 @@
-import { cookies } from 'next/headers'
-
-import { Blur } from '@/components/Blur'
-import { Copyright } from '@/components/Copyright'
 import { EmptyMemories } from '@/components/EmptyMemories'
-import { Hero } from '@/components/Hero'
-import { SignIn } from '@/components/SignIn'
-import { Stripes } from '@/components/Stripes'
-import { Profile } from '@/components/Profile'
 
 export default function Home() {
-  const isAuthenticated = cookies().has('token')
-
-  return (
-    <main className="grid min-h-screen grid-cols-2">
-      <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
-        <Blur />
-        <Stripes />
-
-        {isAuthenticated ? <Profile /> : <SignIn />}
-        <Hero />
-        <Copyright />
-      </div>
-
-      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <EmptyMemories />
-      </div>
-    </main>
-  )
+  return <EmptyMemories />
 }
